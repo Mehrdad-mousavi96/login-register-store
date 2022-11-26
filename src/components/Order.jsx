1eedad9b207f0767717e68a0a699712a406437ce
mehrdad <mehrdad.fmousavi@gmail.com>
2022-11-26 19:16:15 +0330

    adding order component to show products

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Order = () => {
+const Order = ({ order }) => {
   return (
-    <div>Order</div>
-  )
-}
+    <tbody className={order.isPaymentCompleted ? 'bg-sky-200 hover:bg-sky-300' : 'bg-emerald-300 hover:bg-emerald-400'}>
+      <tr className="border-b">
+        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+          {order.id}
+        </td>
+        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+          {order.productId}
+        </td>
+        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+          {order.quantity}
+        </td>
+        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+          {order.isPaymentCompleted ? (
+            <p className="font-semibold" >Paid</p>
+          ) : (
+            <a className="text-sky-900 underline cursor-pointer">
+              Payment Gateway
+            </a>
+          )}
+        </td>
+      </tr>
+    </tbody>
+  );
+};
 
-export default Order
+export default Order;
